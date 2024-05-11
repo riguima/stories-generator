256242373dd97b125ba9ef1ede8ba985b921ec32
riguima <richard.alexsander.guima@gmail.com>
2024-05-11 11:55:38 -0300

    feat: now can change background image, edit plans and search for members

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from importlib import import_module
+from pathlib import Path
 
 import telebot
 import toml
@@ -7,7 +8,8 @@ from telebot.util import quick_markup, update_types
 
 from stories_generator.config import config
 from stories_generator.database import Session
-from stories_generator.models import TelegramUser
+from stories_generator.models import Signature, TelegramUser
+from stories_generator.utils import get_today_date
 
 bot = telebot.TeleBot(config['BOT_TOKEN'])
 
@@ -34,8 +36,12 @@ def start(message):
             'Gerar Imagens': {'callback_data': 'generate_images'},
         }
         if message.chat.id in config['ADMINS']:
+            options['Assinantes'] = {'callback_data': 'show_subscribers'}
             options['Editar Mensagem do Menu'] = {
                 'callback_data': 'edit_menu_message'
+            }
+            options['Editar Imagem Padrão'] = {
+                'callback_data': 'edit_background_image'
             }
             options['Adicionar Plano'] = {'callback_data': 'add_plan'}
             options['Planos'] = {'callback_data': 'show_plans'}
@@ -50,6 +56,29 @@ def start(message):
         bot.send_message(
             message.chat.id,
             'Adicione um arroba para sua conta do Telegram para utilizar esse bot',
+        )
+
+
+@bot.callback_query_handler(func=lambda c: c.data == 'show_subscribers')
+def show_subscribers(callback_query):
+    with Session() as session:
+        users = session.scalars(select(TelegramUser)).all()
+        actives = 0
+        for user_model in users:
+            query = (
+                select(Signature)
+                .where(Signature.due_date >= get_today_date())
+                .where(Signature.user_id == user_model.id)
+            )
+            signatures_models = session.scalars(query).all()
+            if signatures_models:
+                actives += 1
+        bot.send_message(
+            callback_query.message.chat.id,
+            f'Assinantes: {len(users)}\nAtivos: {actives}',
+            reply_markup=quick_markup(
+                {'Voltar': {'callback_data': 'return_to_main_menu'}}
+            ),
         )
 
 
@@ -68,6 +97,34 @@ def on_menu_message(message):
     toml.dump(config, open('.config.toml', 'w'))
     bot.send_message(message.chat.id, 'Mensagem Editada!')
     start(message)
+
+
+@bot.callback_query_handler(func=lambda c: c.data == 'edit_background_image')
+def edit_background_image(callback_query):
+    bot.send_message(
+        callback_query.message.chat.id, 'Envie a nova imagem de fundo'
+    )
+    bot.register_next_step_handler(callback_query.message, on_background_image)
+
+
+def on_background_image(message):
+    if message.document:
+        image = bot.get_file(message.document.file_id)
+        valid_extensions = ['jpeg', 'jpg', 'png']
+        if image.file_path.split('.')[-1].lower() not in valid_extensions:
+            bot.send_message(
+                message.chat.id, 'Imagem inválida, tente novamente'
+            )
+            bot.register_next_step_handler(message, on_background_image)
+            return
+        image_file = bot.download_file(image.file_path)
+        with open(Path('static') / 'background.png', 'wb') as f:
+            f.write(image_file)
+        bot.send_message(message.chat.id, 'Imagem de Fundo Alterada!')
+        start(message)
+    else:
+        bot.send_message(message.chat.id, 'Imagem inválida, tente novamente')
+        bot.register_next_step_handler(message, on_background_image)
 
 
 @bot.callback_query_handler(func=lambda c: c.data == 'return_to_main_menu')
